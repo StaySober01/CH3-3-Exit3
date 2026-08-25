bce0b471eacb846c8951d4e312c394c51a1572d9
한성준 <gggf0817@naver.com>
2026-08-25 19:29:45 +0900

    chore: initialize Unreal Engine project

---
+++ Exit3/Source/Exit3/Exit3.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Exit3.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Exit3, "Exit3" );
